babe05333b7e8ed525a81cd45ab236b13615e7f4
Drees Camertijn <dreescamertijn@hotmail.com>
2019-01-29 11:24:32 +0100

    app.js mqtt is finicht

--- App.js
+++ App.js
@@ -1,2 +1,27 @@
-//mqtt
+// Wait for the document to be fully loaded so that all 
+// context is available before running any code
 
+document.addEventListener("DOMContentLoaded",function(){
+
+  const topic = 'webtechnology/Ambient-Observer/Ambient'
+
+  Vue.use(VueMqtt.default, 'ws://mqtt.labict.be:1884')
+
+  // Create a new Vue application. 
+  new Vue({
+    el: '#app',         // the element on which the Vue app must be build on
+    data: {             // the application contains some data
+      Ambient: {}       // some weather information, but we do not have any values yet
+    },
+    mounted () {
+      this.$mqtt.subscribe(topic)       // subscribe to the information topic on MQTT
+    },
+    mqtt: {
+      [topic]: function(message) {        // when a message arrives on our topic
+        let json = (new TextDecoder("utf-8").decode(message))    // we need to convert (decode) the byte information to a text string
+        let data = JSON.parse(json)       // parse the JSON format to an object
+        this.$data.Ambient = data         // 
+      }
+    }
+  });
+});
